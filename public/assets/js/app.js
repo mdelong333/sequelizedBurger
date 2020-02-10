f10a2c19e0f00e6bfb740094ee7a5acc35dc0302
murnda <mdelong333@gmail.com>
2020-02-10 17:49:46 -0500

    added prevent default to click

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(".make").on("click", function(event) {
-        // event.preventDefault();
+        event.preventDefault();
         // console.log("click");
         var newBurg = {
             name: $("#burger-entry").val().trim(),
